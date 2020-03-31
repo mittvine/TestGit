8c47de5fbaa34dbc04a739946c72191575831574
mittvine <mittvine@gmail.com>
2020-03-31 17:21:41 +0530

    Fourth commit

--- src/test/java/qaclickacademy/RestApiTest.java
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -10,10 +10,17 @@ public class RestApiTest {
 	{
 		System.out.println("postman");
 		System.out.println("postman2");
+		System.out.println("postman3");
+		System.out.println("postman4");
+		System.out.println("postman5");
 	}
 	
 	@Test
 	public void RestAPI(){
+		System.out.println("RestAPI");
+	}
+	@Test
+	public void RestAPII(){
 		System.out.println("RestAPI");
 	}
 
